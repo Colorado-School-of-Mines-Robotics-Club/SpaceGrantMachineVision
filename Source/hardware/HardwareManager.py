4794ffe9688929e32076d2f835a0cb3848efc63c
Cattym33 <mmccat3@gmail.com>
2022-02-18 19:37:27 -0700

    Write PWM function implemented

--- Source/hardware/HardwareManager.py
+++ Source/hardware/HardwareManager.py
@@ -53,15 +53,138 @@ class HardwareManager:
 
     def write_pwm(self, motor1, motor2, motor3, motor4, sus1, sus2, sus3, sus4, wheel1, wheel2, wheel3, wheel4, dir1,
                   dir2, dir3, dir4, rgb):
+        writes = []
+
+        writes_counter = 0
         #TODO: Write PWM for each motor
+        for reg in motor_reg:
+            #four writes for each register
+            #writes holds time high, use it to calculate time low
+            high = writes[writes_counter]
+            low = 4095 - high
+
+            #calculate 1st byte for high, high is a number between 0-4095 (which takes up 12 bits max)
+            #1st bit holds biggest 4 bits of high, 2nd bit holds last 8 (4 + 8 = 12 bits stored)
+            #an int is stored using 4 bits, so to isolate those 4, shift 20 left, then 28 right
+            high1 = high
+            high1 << 20
+            high1 >> 28
+            #high1 is now the first 4 isolated bits
+            #high2 needs to be the last 8 bits isolated, so shift left 24, right 24 to get rid of all leading bits
+            high2 = high
+            high2 << 24
+            high2 >> 24
+
+            #same process for calculating the low data
+            low1 = low
+            low1 << 20
+            low1 >> 28
+
+            low2 = low
+            low2 << 24
+            low2 >> 24
+
+            #write the 4 registers, data = [reg, byte_to_write]
+            data = [reg[0][0], high1] #first register is the first byte of high
+            bus.write_I2C_block_data(pwm_address, 0, data)
+            data = [reg[0][1], high2]#second register is the second byte of high
+            bus.write_IC2_block_data(pwm_address, 0, data)
+            data = [reg[1][0], low1]#3rd register is the first byte of low
+            bus.write_IC2_block_data(pwm_address, 0, data)
+            data = [reg[1][1], low2]#4th register is the second byte of low
+            bus.write_IC2_block_data(pwm_address, 0, data)
+
+            #increment the writes_counter by one; only one value in the writes array was used
+            writes_counter += 1
 
         #TODO: Write PWM for each servo
+        for reg in servo_reg:
+            # four writes for each register
+            # writes holds time high, use it to calculate time low
+            high = writes[writes_counter]
+            low = 4095 - high
+
+            # calculate 1st byte for high, high is a number between 0-4095 (which takes up 12 bits max)
+            # 1st bit holds biggest 4 bits of high, 2nd bit holds last 8 (4 + 8 = 12 bits stored)
+            # an int is stored using 4 bits, so to isolate those 4, shift 20 left, then 28 right
+            high1 = high
+            high1 << 20
+            high1 >> 28
+            # high1 is now the first 4 isolated bits
+            # high2 needs to be the last 8 bits isolated, so shift left 24, right 24 to get rid of all leading bits
+            high2 = high
+            high2 << 24
+            high2 >> 24
+
+            # same process for calculating the low data
+            low1 = low
+            low1 << 20
+            low1 >> 28
+
+            low2 = low
+            low2 << 24
+            low2 >> 24
+
+            # write the 4 registers, data = [reg, byte_to_write]
+            data = [reg[0][0], high1]  # first register is the first byte of high
+            bus.write_I2C_block_data(pwm_address, 0, data)
+            data = [reg[0][1], high2]  # second register is the second byte of high
+            bus.write_IC2_block_data(pwm_address, 0, data)
+            data = [reg[1][0], low1]  # 3rd register is the first byte of low
+            bus.write_IC2_block_data(pwm_address, 0, data)
+            data = [reg[1][1], low2]  # 4th register is the second byte of low
+            bus.write_IC2_block_data(pwm_address, 0, data)
+
+            # increment the writes_counter by one; only one value in the writes array was used
+            writes_counter += 1
+
         #TODO: write PWM for each LED
+        for reg in LED_reg:
+            # four writes for each register
+            # writes holds time high, use it to calculate time low
+            high = writes[writes_counter]
+            low = 4095 - high
+
+            # calculate 1st byte for high, high is a number between 0-4095 (which takes up 12 bits max)
+            # 1st bit holds biggest 4 bits of high, 2nd bit holds last 8 (4 + 8 = 12 bits stored)
+            # an int is stored using 4 bits, so to isolate those 4, shift 20 left, then 28 right
+            high1 = high
+            high1 << 20
+            high1 >> 28
+            # high1 is now the first 4 isolated bits
+            # high2 needs to be the last 8 bits isolated, so shift left 24, right 24 to get rid of all leading bits
+            high2 = high
+            high2 << 24
+            high2 >> 24
+
+            # same process for calculating the low data
+            low1 = low
+            low1 << 20
+            low1 >> 28
+
+            low2 = low
+            low2 << 24
+            low2 >> 24
+
+            # write the 4 registers, data = [reg, byte_to_write]
+            data = [reg[0][0], high1]  # first register is the first byte of high
+            bus.write_I2C_block_data(pwm_address, 0, data)
+            data = [reg[0][1], high2]  # second register is the second byte of high
+            bus.write_IC2_block_data(pwm_address, 0, data)
+            data = [reg[1][0], low1]  # 3rd register is the first byte of low
+            bus.write_IC2_block_data(pwm_address, 0, data)
+            data = [reg[1][1], low2]  # 4th register is the second byte of low
+            bus.write_IC2_block_data(pwm_address, 0, data)
+
+            # increment the writes_counter by one; only one value in the writes array was used
+            writes_counter += 1
+
         self.write_GPIO(self, dir1, dir2,dir3,dir4)
 
     def write_gpio(self, dir1, dir2, dir3, dir4):
         dir1 = 1
         #TODO: write the output pins for the directions
+
 
     def read_motor(self, thread):
         #read current edge value before starting the loop
@@ -84,7 +207,16 @@ class HardwareManager:
             self.past_encoders[thread] = self.curr_encoders[thread][0]
 
     def read_servo(self):
-        self.curr_servo[0] = 1
+        while True:
+            self.curr_servo[0] = GPIO.input(self.servo_pins[0])
+            self.curr_servo[1] = GPIO.input(self.servo_pins[1])
+            self.curr_servo[2] = GPIO.input(self.servo_pins[2])
+            self.curr_servo[3] = GPIO.input(self.servo_pins[3])
+            self.curr_servo[4] = GPIO.input(self.servo_pins[4])
+            self.curr_servo[5] = GPIO.input(self.servo_pins[5])
+            self.curr_servo[6] = GPIO.input(self.servo_pins[6])
+            self.curr_servo[7] = GPIO.input(self.servo_pins[7])
+            
 
     def read_accelerometer(self):
         self.curr_accel[0] = 1
