397060a4cbdf254c922e490c650002bdc14cc052
justincdavis <jcdavis@mssm.org>
2022-03-04 12:00:13 -0700

    object detection aot compiled

--- Code/source/__init__.py
+++ Code/source/__init__.py
@@ -2,7 +2,9 @@ import numpy as np
 import cv2
 from .features import compile_features
 from .kinematics import compile_kinematics
+from .objectDetection import compile_object_detection
 
 
 compile_features()
 compile_kinematics()
+compile_object_detection()
