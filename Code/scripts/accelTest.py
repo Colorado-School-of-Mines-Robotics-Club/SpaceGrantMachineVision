6c55d6b8ca095773bbd82068f593a55308cede8e
justincdavis <jcdavis@mssm.org>
2022-03-11 19:01:23 -0700

    acceleratometer testing script

---
+++ Code/scripts/accelTest.py
@@ -0,0 +1,9 @@
+from smbus2 import *
+
+bus = SMBus(1)
+accelerometer_address = 0x3B
+accel_reg = 0x32
+
+while True:
+    data = bus.read_i2c_block_data(accelerometer_address, accel_reg, 6)
+    print(data)
