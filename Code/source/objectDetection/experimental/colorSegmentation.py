16c72f954e58c3cd26f259a4c5b882ebc68954f7
justincdavis <jcdavis@mssm.org>
2022-04-06 17:55:30 -0600

    changing to sklearn

--- Code/source/objectDetection/experimental/colorSegmentation.py
+++ Code/source/objectDetection/experimental/colorSegmentation.py
@@ -4,32 +4,37 @@ import time
 # Additional libs
 import cv2
 import numpy as np
-from sklearn.cluster import DBSCAN
+from sklearn.cluster import MiniBatchKMeans, KMeans, DBSCAN
 
 # Custom  imports
 from source.cameras.DisplayManager import DisplayManager
 
 
 # https://www.kdnuggets.com/2019/08/introduction-image-segmentation-k-means-clustering.html
-def segmentColors(image: np.ndarray, method='kmeans', K=5, iterations=10, attempts=10, downscale=True,
+def segmentColors(image: np.ndarray, method='minibatchkmeans', K=5, iterations=10, attempts=10, downscale=True,
                   downscaleRatio=0.4, downscaleMethod='linear', show=False, threadedDisplay=False) -> np.ndarray:
-    method_list = ['kmeans']
-    assert method in method_list
+    cluster_method_dict = {
+        'minibatchkmeans': MiniBatchKMeans,
+        'kmeans': KMeans,
+        'dbscan': DBSCAN
+    }
+    assert method in cluster_method_dict
+    cluster_method = cluster_method_dict[method]
 
-    methods_dict = {
+    resize_method_dict = {
         'nearest': cv2.INTER_NEAREST,
         'linear': cv2.INTER_LINEAR,
         'area': cv2.INTER_AREA,
         'cubic': cv2.INTER_CUBIC
     }
-    assert downscaleMethod in methods_dict
+    assert downscaleMethod in resize_method_dict
 
     resized_image = cv2.GaussianBlur(image, (3, 3), cv2.BORDER_DEFAULT)
     if downscale:
         width = int(image.shape[1] * downscaleRatio)
         height = int(image.shape[0] * downscaleRatio)
         dim = (width, height)
-        resized_image = cv2.resize(image, dim, methods_dict[downscaleMethod])
+        resized_image = cv2.resize(image, dim, resize_method_dict[downscaleMethod])
 
     img = cv2.cvtColor(resized_image, cv2.COLOR_BGR2HSV)
     vectorized = np.float32(img.reshape((-1, 3)))
@@ -44,7 +49,7 @@ def segmentColors(image: np.ndarray, method='kmeans', K=5, iterations=10, attemp
         width = int(image.shape[1])
         height = int(image.shape[0])
         dim = (width, height)
-        result_image = cv2.resize(result_image, dim, methods_dict[downscaleMethod])
+        result_image = cv2.resize(result_image, dim, resize_method_dict[downscaleMethod])
 
     if show:
         displayImg = np.concatenate((image, result_image), axis=1)
