278661af43067f56505e414430f49dc6f7d1ba3a
justincdavis <jcdavis@mssm.org>
2022-04-06 12:23:51 -0600

    initial implementation of color quant using kmeans, adding DBSCAN

---
+++ Code/source/objectDetection/experimental/colorSegmentation.py
@@ -0,0 +1,57 @@
+# Built in python libs
+import time
+
+# Additional libs
+import cv2
+import numpy as np
+
+# Custom  imports
+from source.cameras.DisplayManager import DisplayManager
+
+
+# https://www.kdnuggets.com/2019/08/introduction-image-segmentation-k-means-clustering.html
+def segmentColors(image: np.ndarray, method='kmeans', K=3, iterations=5, downscale=True, downscaleRatio=0.5,
+                  downscaleMethod='linear', show=False, threadedDisplay=False) -> np.ndarray:
+    startTime = time.perf_counter()
+
+    method_list = ['kmeans']
+    assert method in method_list
+
+    methods_dict = {
+        'nearest': cv2.INTER_NEAREST,
+        'linear': cv2.INTER_LINEAR,
+        'area': cv2.INTER_AREA,
+        'cubic': cv2.INTER_CUBIC
+    }
+    assert downscaleMethod in methods_dict
+
+    resized_image = image
+    if downscale:
+        width = int(image.shape[1] * downscaleRatio)
+        height = int(image.shape[0] * downscaleRatio)
+        dim = (width, height)
+        resized_image = cv2.resize(image, dim, methods_dict[downscaleMethod])
+
+    img = cv2.cvtColor(resized_image, cv2.COLOR_BGR2RGB)
+    vectorized = np.float32(img.reshape((-1, 3)))
+    if method == 'kmeans':
+        criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+        ret, label, center = cv2.kmeans(vectorized, K, None, criteria, iterations, cv2.KMEANS_PP_CENTERS)
+        center = np.uint8(center)
+        res = center[label.flatten()]
+        result_image = cv2.cvtColor(res.reshape(img.shape), cv2.COLOR_RGB2BGR)
+
+    if downscale:
+        width = int(image.shape[1])
+        height = int(image.shape[0])
+        dim = (width, height)
+        result_image = cv2.resize(result_image, dim, methods_dict[downscaleMethod])
+
+    if show:
+        displayImg = np.concatenate((image, result_image), axis=1)
+        if threadedDisplay:
+            DisplayManager.show(f"segmentColors using {method}", displayImg)
+        else:
+            cv2.imshow(f"segmentColors using {method}", displayImg)
+
+    return result_image
