a62ba936c5199f30d0c8ed92741bbba312755356
kvembar <74742280+kvembar@users.noreply.github.com>
2022-04-07 19:32:25 -0600

    Turned route into directives using instruction_converter, turned node directions to actual distance using convert_route_to_dist, and made note of starting and ending points in plot_graph call

--- Code/tester.py
+++ Code/tester.py
@@ -5,14 +5,16 @@ import numpy as np
 
 test_grid = Map()
 
-for i in range(3000):
+for i in range(1000):
     test_grid.assign_rand()
 
 obstacles = test_grid.get_grid()
 passable_tiles = test_grid.get_passable()
 
-route = astar(obstacles, (0,0), (len(obstacles) - 1,len(obstacles[0]) - 1), passable = passable_tiles)
+route = astar(obstacles, (len(obstacles) - 1,len(obstacles[0])//2), (0,len(obstacles[0])//2), passable = passable_tiles)
 
-print(route)
 if(route is not False):
-    print(test_grid.convert_route_to_dist(route))
+    print(Map.instruction_converter(route, test_grid.DPerNode))
+    print("\n")
+    print(Map.instruction_converter(route, test_grid.DPerNode, compress = True))
+    plot_graph(obstacles, (len(obstacles) - 1,len(obstacles[0])//2), (0,len(obstacles[0])//2), route)
