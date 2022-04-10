6055bfa119b2f6edaa65699111ec44fbd373e961
justincdavis <jcdavis@mssm.org>
2022-04-09 18:23:51 -0600

    added LED passthrough to interface

--- Code/source/hardware/KinematicHardwareInterface.py
+++ Code/source/hardware/KinematicHardwareInterface.py
@@ -14,9 +14,18 @@ except ModuleNotFoundError as e:
 
 
 class KinematicHardwareInterface:
-    def __init__(self):
+    def __init__(self, robotData: Union[RobotData, None] = None, ledStates: Union[List[bool], None] = None):
         self.kinematicModel = KinematicModel()
         self.robotData = RobotData(0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
+        if robotData is not None:
+            self.robotData = robotData
+        self.ledStates = [False, False, False, False]
+        if ledStates is not None:
+            assert len(ledStates) == 4
+            self.ledStates = ledStates
+        self.motorServo = None
+        self.command = None
+        self.updateFromRobotData()  # sets up the initial command
 
     @staticmethod
     def ms_to_pwm(ms: float) -> int:
@@ -38,9 +47,21 @@ class KinematicHardwareInterface:
         # TODO
         return 0
 
+    @staticmethod
+    def bool_to_pwm(boolean: bool) -> int:
+        if boolean:
+            return 4095
+        return 0
+
+    @staticmethod
+    def pwm_to_bool(pwm: int) -> bool:
+        if pwm > 0:
+            return True
+        return False
+
     # Will take robotData parameters and run them through the kinematic model
     # will return m/s for motors and angles in radians for servos
-    def updateFromRobotData(self, robotData: Union[RobotData, None] = None) -> List[Union[float, int]]:
+    def updateFromRobotData(self, robotData: Union[RobotData, None] = None) -> None:
         if robotData is not None:
             self.robotData = robotData
         self.kinematicModel.update(wheelVelocities=[self.robotData.linear for i in range(4)],
@@ -57,7 +78,21 @@ class KinematicHardwareInterface:
             output_data[idx] = ws
             output_data[idx + 4] = wa
             output_data[idx + 5] = sa
-        return output_data
+        self.motorServo = output_data
+        self.command = self.motorServo + self.ledStates
+
+    # takes a List of length 4 with true false states
+    def updateLEDs(self, ledStates: List[bool]):
+        assert len(ledStates) == 4
+        self.ledStates = ledStates
+        self.command = self.motorServo + self.ledStates
+
+    # gets the pwms for motors and servos from our interface, does not have leds
+    def getCommandPWM(self) -> List[int]:
+        motor_pwms = [self.ms_to_pwm(com) for com in self.command[0:4]]
+        servo_pwms = [self.radians_to_pwm(com) for com in self.command[4:-1]]
+        led_pwms = [self.bool_to_pwm(com) for com in self.ledStates]
+        return motor_pwms + servo_pwms + led_pwms
 
 # pwms
 #   'linear': pwm to assign all motors
