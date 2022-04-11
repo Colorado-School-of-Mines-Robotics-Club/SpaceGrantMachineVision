eef4954a0daefb6b72606a85a11c1cbaba9bfa7e
justincdavis <davis.justin@mssm.org>
2022-04-10 23:07:18 -0600

    added the script

---
+++ Scripts/hardwareTester.py
@@ -0,0 +1,5 @@
+from Code.source.hardware.HardwareManager import HardwareManager
+from Code.source.hardware.KinematicHardwareInterface import KinematicHardwareInterface
+
+manager = HardwareManager()
+interface = KinematicHardwareInterface()
