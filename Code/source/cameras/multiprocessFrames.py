7d9e84f66f92c8a1dbce8ec34f792cb09e20fee0
justincdavis <jcdavis@mssm.org>
2022-04-12 19:35:35 -0600

    additional

--- Code/source/cameras/multiprocessFrames.py
+++ Code/source/cameras/multiprocessFrames.py
@@ -13,4 +13,15 @@ from openVO import StereoCamera
 def PTframes(args: Tuple) -> Tuple[np.ndarray, np.ndarray]:
     queue, stereo, left_capture, right_capture = args
 
-    pass
+    got_left_frame = left_capture.grab()
+    left_frame_time = time.perf_counter()
+
+    got_right_frame = right_capture.grab()
+    right_frame_time = time.perf_counter()
+
+    if got_left_frame and got_right_frame:
+        left_image = left_capture.retrieve()
+        right_image = right_capture.retrieve()
+
+
+    return (None, left_frame_time), (None, right_frame_time)
